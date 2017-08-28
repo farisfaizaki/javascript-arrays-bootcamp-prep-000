81ea935951d8e7995db2a9b58b00b0a16af437e2
Akhmad Faris Faizaki <faris.faizaki@gmail.com>
2017-08-28 15:04:07 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,9 +1,7 @@
 var chocolateBars = ['snickers','hundred grand','kitkat','skittles'];
 
 function addElementToBeginningOfArray(array,element){
-  var newArray = new Array();
-  nerArray = [element,...array];
-  return newArray;
+  return [element,...array];
 }
 
 function destructivelyAddElementToBeginningOfArray(array,element){
